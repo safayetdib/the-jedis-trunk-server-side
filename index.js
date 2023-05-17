cf044efbf82130d257ce8855c9e9f2d55587c402
safayet <safayetdib@gmail.com>
2023-05-17 21:59:20 +0600

    added connection to mongodb

--- index.js
+++ index.js
@@ -9,6 +9,33 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// MongoDB
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dc2rluc.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+});
+
+async function run() {
+	try {
+		await client.connect();
+
+		await client.db('admin').command({ ping: 1 });
+		console.log(
+			'Pinged your deployment. You successfully connected to MongoDB!'
+		);
+	} finally {
+		// await client.close();
+	}
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
 	res.send('A long time ago, in a galaxy far, far away...');
 });
